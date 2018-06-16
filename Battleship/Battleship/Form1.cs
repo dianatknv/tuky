3c4579d0b3fd91c274153fb6960aa276c8f19404
diana <35858793+dianatknv@users.noreply.github.com>
2018-06-16 19:42:45 +0600

    Add files via upload

---
+++ Battleship/Battleship/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Battleship
+{
+    public partial class Form1 : Form
+    {
+        GameLogic gl = new GameLogic();
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.Controls.Add(gl.p1);
+            this.Controls.Add(gl.p2);
+            gl.p1.Enabled = true;
+            gl.p2.Enabled = false;
+        }
+
+        /* public void GameOver()
+         {
+             if (gl.p1.brain.k == 0)
+             {
+                 MessageBox.Show("Game over!");
+             }
+
+             if(gl.p2.brain.k == 0)
+             {
+                 MessageBox.Show("You won!");
+             }
+         }*/
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
