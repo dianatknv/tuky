9f178e45a7fe7575d9a69150702e4e874ac876fb
diana <35858793+dianatknv@users.noreply.github.com>
2018-06-16 19:56:33 +0600

    Add files via upload

---
+++ FarManager/EmptyClass.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace FarManager
+{
+    public class EmptyClass
+    {
+        public EmptyClass()
+        {
+        }
+    }
+}
---
+++ FarManager/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FarManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.CursorVisible = false;
+            FAR far = new FAR(@"/Users/diana/Documents");
+            
+            bool quit = false;
+
+            while (!quit)
+            {
+                far.Draw();
+                ConsoleKeyInfo pressedKey = Console.ReadKey();
+                switch (pressedKey.Key)
+                {
+                    case ConsoleKey.Escape:
+                        quit = true;
+                        break;
+                    case ConsoleKey.UpArrow:
+                    case ConsoleKey.DownArrow:
+                    case ConsoleKey.Backspace:
+                    case ConsoleKey.Enter:
+                        far.Process(pressedKey);
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
